4867ebb3d9432d7dcf2a78d70da761cfa6cece70
Arthur Edelstein <arthuredelstein@gmail.com>
2011-01-17 09:25:49 -0800

    clarify

--- src/var_finder/core.clj
+++ src/var_finder/core.clj
@@ -16,20 +16,20 @@
 			
 (defn read-clojure-source
   "Takes the path to a clojure source file and returns a sequence
-  of maps, each containing an s-expression, the corresponding lines of text
+  of maps, each containing an s-expression, the corresponding lines of code
   and the line number."
   [file]
   (let [text (slurp file)
         code-reader (LineNumberingPushbackReader. (StringReader. (str \newline text)))
         text-reader (BufferedReader. (StringReader. (str text)))]
     (take-while identity (repeatedly
-      (fn [] (let [ntop (.getLineNumber code-reader)
+      (fn [] (let [nbotlast (.getLineNumber code-reader)
                    sexpr (try (read code-reader) (catch Exception e nil))
                    nbot (.getLineNumber code-reader)
-                   all-lines (repeatedly (- nbot ntop) #(.readLine text-reader))
-                   code-lines (drop-while empty-line? all-lines)
-                   nlines (count code-lines)]
-               (if sexpr {:sexpr sexpr, :text-lines code-lines, :line (- nbot nlines)})))))))
+                   latest-lines (repeatedly (- nbot nbotlast) #(.readLine text-reader))
+                   code-lines (drop-while empty-line? latest-lines)
+                   line (- nbot (count code-lines))]
+               (if sexpr {:sexpr sexpr, :code-lines code-lines, :line line})))))))
 			   
 ;; tests
 
