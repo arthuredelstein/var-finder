f710d9dec9fcb769e1bda3e6589a88db95fd9ade
Arthur Edelstein <arthuredelstein@gmail.com>
2011-01-17 16:55:40 -0800

    build-var-info etc.

--- src/var_finder/core.clj
+++ src/var_finder/core.clj
@@ -16,8 +16,8 @@
 			
 (defn read-clojure-source
   "Takes the path to a clojure source file and returns a sequence
-  of maps, each containing an s-expression, the corresponding lines of code
-  and the line number."
+  of maps, each containing an s-expression with metadata including
+  the line number and the file name."
   [file]
   (let [text (slurp file)
         code-reader (LineNumberingPushbackReader. (StringReader. (str \newline text)))
@@ -29,14 +29,85 @@
                    latest-lines (repeatedly (- nbot nbotlast) #(.readLine text-reader))
                    code-lines (drop-while empty-line? latest-lines)
                    line (- nbot (count code-lines))]
-               (if sexpr {:sexpr sexpr, :code-lines code-lines, :line line})))))))
-			   
+               (if sexpr (with-meta sexpr {:line line :file file}))))))))
+
+
+;; namespace: { :full-name :short-name :doc :author :members :subspaces :see-also}
+;; vars: {:name :doc :arglists :var-type :file :line :added :deprecated :dynamic}
+;; (merge (select-keys (meta v) [:arglists :file :line :added :deprecated :dynamic])
+
+(defn get-meta-deflike
+  [sexpr]
+  (meta (second sexpr)))
+
+;; TODO: :arglists
+(defn get-meta-defnlike
+  [sexpr]
+  (let [[_ t2 t3 t4] sexpr
+        d (if (string? t3) t3)]
+    (merge
+      (meta t2)
+      (if (map? t3) t3)
+      (if (and d (map? t4)) t4)
+      (if d {:doc d}))))
+
+(defn get-meta-tail-doc
+  [sexpr n]
+  (merge
+    (get-meta-deflike sexpr)
+    (let [[_ _ t3 t4] sexpr]
+      {:doc
+        (condp = n
+          3 t3
+          4 t4)})))
+
+;; TODO: 'ns (namespaces)
+(defn analyze-sexpr
+	"Analyze the s-expression for docs and metadata."
+  [sexpr]
+    (condp #(some #{%2} %1) (first sexpr)
+      '(def defhinted defonce defstruct)
+        (get-meta-deflike sexpr)
+      '(defn definline defmacro defmulti defn-memo defnk)
+        (get-meta-defnlike sexpr)
+      '(defprotocol defunbound)
+        (get-meta-tail-doc 3)
+      '(defalias defvar)
+        (get-meta-tail-doc 4)
+      nil))
+
+(defn get-var-type [sexpr]
+  (or
+    {'defn      "function"
+     'definline "function"
+     'defmacro  "macro"
+     'defmulti  "multimethod"
+     'defnmemo  "function"
+     'defnk     "function"}
+       (first sexpr)
+    "var"))
+      
+(defn build-var-info [sexpr]
+  (merge
+    (select-keys (meta sexpr) [:file :line])
+    (select-keys (analyze-sexpr sexpr) [:arglists :doc :added :deprecated :dynamic])
+    {:name (name (second sexpr))
+     :var-type (get-var-type sexpr)}))
+
+
+(defn collect-vars [sexprs]
+  (map build-var-info sexprs))
+			  
 ;; tests
 
 (def test-file
-  "https://github.com/clojure/clojure/raw/b578c69d7480f621841ebcafdfa98e33fcb765f6/src/clj/clojure/core.clj")
+  ;"https://github.com/clojure/clojure/raw/b578c69d7480f621841ebcafdfa98e33fcb765f6/src/clj/clojure/core.clj")
+  "src/var_finder/code_sample.clj")
 
 (defn test-read []
   (read-clojure-source test-file))
+  
+(defn test-collect []
+  (collect-vars (test-read)))
 
   
