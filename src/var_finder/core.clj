cc2e8e91658b57e0a5f392569c2a2159cf7f9fe4
Arthur Edelstein <arthuredelstein@gmail.com>
2011-01-18 22:47:11 -0800

    include source text in meta map because we can

--- src/var_finder/core.clj
+++ src/var_finder/core.clj
@@ -17,7 +17,7 @@
 (defn read-clojure-source
   "Takes the path to a clojure source file and returns a sequence
   of maps, each containing an s-expression with metadata including
-  the line number and the file name."
+  the line number, the file name and the source text."
   [file]
   (let [text (slurp file)
         code-reader (LineNumberingPushbackReader. (StringReader. (str \newline text)))
@@ -29,7 +29,9 @@
                    latest-lines (repeatedly (- nbot nbotlast) #(.readLine text-reader))
                    code-lines (drop-while empty-line? latest-lines)
                    line (- nbot (count code-lines))]
-               (if sexpr (with-meta sexpr {:line line :file file}))))))))
+               (if sexpr (with-meta sexpr {:line line 
+                                           :file file
+                                           :source code-lines}))))))))
 
 ;; namespace: { :full-name :short-name :doc :author :members :subspaces :see-also}
 ;; vars: {:name :doc :arglists :var-type :file :line :added :deprecated :dynamic}
